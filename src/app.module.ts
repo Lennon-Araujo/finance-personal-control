c51ed48d74bba8cc0b43326920c6fe832de5f7e3
Lennon Araujo <lennon_hih@hotmail.com>
2023-08-25 19:03:31 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [TransactionsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
